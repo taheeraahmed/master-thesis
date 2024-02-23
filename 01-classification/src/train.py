2231c30dc0c023c3f07696a617dd3ee20a8730bc
Taheera Ahmed <taheera@hotmail.com>
2024-02-23 11:41:15 +0100

    remove task argument

--- 01-classification/src/train.py
+++ 01-classification/src/train.py
@@ -36,7 +36,6 @@ def train(args):
 
 if __name__ == "__main__":
     model_choices = ['densenet', 'swin']
-    task_choices = ['multi-class']
     # TODO: Rename bce to wbce and focal-loss to wfl
     loss_choices = ['bce', 'focal-loss']
 
