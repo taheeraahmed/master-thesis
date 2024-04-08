669d7d67b6dd144c53600949fec1613f30fe4782
Taheera Ahmed <taheera@hotmail.com>
2024-04-09 00:39:06 +0200

    add transforms

--- 01-multi-label/src/multi_label.py
+++ 01-multi-label/src/multi_label.py
@@ -2,18 +2,47 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from torch.utils.data import DataLoader
-from torchvision.transforms import (Compose,
-                                    Normalize,
-                                    RandomHorizontalFlip,
-                                    Resize,
-                                    ToTensor)
-from torchvision.transforms import InterpolationMode    
+from torchvision import transforms   
 
 from utils import FileManager
 from models import ModelConfig
 from trainers import MultiLabelLightningModule
 from data import ChestXray14HFDataset
 
+import copy
+import random
+import numbers
+from collections.abc import Sequence
+import warnings
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+
+from PIL import ImageFilter
+
+
+class GaussianBlur(object):
+    """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
+
+    def __init__(self, sigma=[.1, 2.]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return x
+
+
+def _setup_size(size, error_msg):
+    if isinstance(size, numbers.Number):
+        return int(size), int(size)
+
+    if isinstance(size, Sequence) and len(size) == 1:
+        return size[0], size[0]
+
+    if len(size) != 2:
+        raise ValueError(error_msg)
+
+    return size
 
 def train_and_evaluate_model(model_config: ModelConfig, file_manager: FileManager, train_df, val_df, test_df) -> None:
     """
@@ -35,19 +64,25 @@ def train_and_evaluate_model(model_config: ModelConfig, file_manager: FileManage
         train_df = train_df.head(train_subset_size)
         val_df = val_df.head(val_subset_size)
 
-    train_transforms = Compose([
-        Resize((model_config.img_size, model_config.img_size), interpolation=InterpolationMode.BILINEAR, antialias=True),
-        ToTensor(),
-        #Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        #RandomHorizontalFlip(),
+    train_transforms = transforms.Compose([
+        transforms.Resize((model_config.img_size, model_config.img_size), interpolation=transforms.InterpolationMode.BILINEAR, antialias=True),
+        transforms.RandomApply([
+                    transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
+                ], p=0.8),
+        transforms.RandomRotation(degrees=(0, 45)),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     file_manager.logger.info(f"Using these augmentations: {train_transforms}")
 
-    val_transforms = Compose([
-        Resize((model_config.img_size, model_config.img_size), interpolation=InterpolationMode.BILINEAR, antialias=True),
-        ToTensor(),
-        #Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    val_transforms = transforms.Compose([
+        transforms.Resize((model_config.img_size, model_config.img_size), interpolation=transforms.InterpolationMode.BILINEAR, antialias=True),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     train_dataset = ChestXray14HFDataset(
