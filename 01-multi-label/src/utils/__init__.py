99b63b645cc4f6c7f5471c53f1a0099a0a125373
Taheera Ahmed <taheera@hotmail.com>
2024-04-09 00:39:17 +0200

    create dir if not exists

--- 01-multi-label/src/utils/__init__.py
+++ 01-multi-label/src/utils/__init__.py
@@ -1,3 +1,4 @@
 from .set_up import set_up, str_to_bool, calculate_idun_time_left, FileManager
 from .check_gpu import check_gpu
 from .df import get_df
+from .create_dir import create_directory_if_not_exists
