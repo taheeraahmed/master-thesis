9e4211bbfff944cbf71342a3f808d07758f80ea3
Taheera Ahmed <taheera@hotmail.com>
2024-04-10 20:38:58 +0200

    change classifying head of resnet50

--- 01-multi-label/src/models/set_model.py
+++ 01-multi-label/src/models/set_model.py
@@ -9,8 +9,15 @@ def set_model(model_arg: str, num_labels: int):
 
     if model_arg == 'resnet50':
         model = resnet50(weights='IMAGENET1K_V1')
-        model.fc = torch.nn.Linear(in_features=model.fc.in_features, out_features=num_labels, bias=True)
         img_size = int(224*2) 
+
+        model.fc = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(in_features=model.fc.in_features, out_features=128),
+            nn.ReLU(),
+            nn.BatchNorm1d(num_features=128),
+            nn.Linear(128, num_labels),
+        )
     elif model_arg == 'resnet34':
         model = resnet34(weights='IMAGENET1K_V1')
         model.fc = torch.nn.Linear(in_features=model.fc.in_features, out_features=num_labels, bias=True)
@@ -31,6 +38,7 @@ def set_model(model_arg: str, num_labels: int):
 
     elif model_arg == "alexnet": 
         model = alexnet(weights="DEFAULT")
+        img_size = int(224*2) 
     
         model.classifier = nn.Sequential(
             nn.Dropout(p=0.2),
@@ -39,7 +47,6 @@ def set_model(model_arg: str, num_labels: int):
             nn.BatchNorm1d(num_features=128),
             nn.Linear(128, num_labels),
         )
-        img_size = int(224*2) 
     else:
         raise ValueError(f'Invalid model argument: {model_arg}')
         
