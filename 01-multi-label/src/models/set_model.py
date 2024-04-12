b5a236efee866323e8e240d63e4a4e13f1671949
Taheera Ahmed <taheera@hotmail.com>
2024-04-12 08:58:16 +0200

    add densenet121

--- 01-multi-label/src/models/set_model.py
+++ 01-multi-label/src/models/set_model.py
@@ -1,5 +1,5 @@
 from models import ModelConfig
-from torchvision.models import resnet50, resnet34, alexnet, vit_b_16
+from torchvision.models import resnet50, resnet34, alexnet, vit_b_16, densenet121
 import torch
 from torch import nn
 
@@ -17,13 +17,16 @@ def set_model(model_arg: str, num_labels: int):
             nn.BatchNorm1d(num_features=128),
             nn.Linear(128, num_labels),
         )
+
     elif model_arg == 'resnet34':
         model = resnet34(weights='IMAGENET1K_V1')
         model.fc = torch.nn.Linear(in_features=model.fc.in_features, out_features=num_labels, bias=True)
         img_size = int(224*2)     
+
     elif model_arg == 'swin':
         img_size = int(224*2) 
         raise NotImplementedError
+    
     elif model_arg == "vit":
         img_size = int(224) 
         model = vit_b_16()
@@ -46,6 +49,18 @@ def set_model(model_arg: str, num_labels: int):
             nn.BatchNorm1d(num_features=128),
             nn.Linear(128, num_labels),
         )
+    elif model_arg == "densenet121":
+        img_size = int(224*2) 
+        model = densenet121(weights="IMAGENET1K_V1")
+        
+        model.classifier = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(1024, 128),
+            nn.ReLU(),
+            nn.BatchNorm1d(num_features=128),
+            nn.Linear(128, num_labels),
+        )
+
     else:
         raise ValueError(f'Invalid model argument: {model_arg}')
         
