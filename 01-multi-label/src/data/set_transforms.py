d58b39047f6a639412e9ed37895d1979b2d45426
Taheera Ahmed <taheera@hotmail.com>
2024-04-13 15:46:28 +0200

    cleaned up transforms :)

--- 01-multi-label/src/data/set_transforms.py
+++ 01-multi-label/src/data/set_transforms.py
@@ -42,17 +42,11 @@ def set_transforms(model_config: ModelConfig, file_manager: FileManager):
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         # https://github.com/thtang/CheXNet-with-localization/blob/master/train.py#L100
         train_transforms = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.RandomHorizontalFlip(p=0.3),
-            transforms.RandomApply([transforms.RandomRotation(degrees=5)], p=1),
-            #transforms.ColorJitter(brightness=0.4, contrast=0.2, saturation=0.3, hue=0),
-            #transforms.GaussianBlur(1, sigma=(0.1, 2.0)),
+            transforms.Resize(size=(model_config.img_size, model_config.img_size)),
+            transforms.RandomHorizontalFlip(p=0.5),
             transforms.ToTensor(),
-            AdjustGamma(gamma=0.8, gain=1),
-            AdjustSharpness(sharpness_factor=2),
-            normalize,
+            normalize
         ])
-
         val_transforms = transforms.Compose([
             transforms.Resize((model_config.img_size, model_config.img_size), interpolation=transforms.InterpolationMode.BILINEAR, antialias=True),
             transforms.ToTensor(),
