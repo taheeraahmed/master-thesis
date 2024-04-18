17be36761eb45c39c3a0d1037efb975e931bbeb8
Taheera Ahmed <taheera@hotmail.com>
2024-04-18 14:46:15 +0200

    fix random rotation augmentation

--- 01-multi-label/src/data/set_transforms.py
+++ 01-multi-label/src/data/set_transforms.py
@@ -47,7 +47,7 @@ def set_transforms(model_config: ModelConfig, file_manager: FileManager):
             transforms.Resize((256, 256), interpolation=transforms.InterpolationMode.BILINEAR, antialias=True),
             transforms.CenterCrop(model_config.img_size),
             transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomApply([transforms.RandomRotation(degrees=7)], p=0.5),
+            transforms.RandomRotation(degrees=7),
             transforms.ColorJitter(contrast=(0.9, 1.1)),
             transforms.ToTensor(),
             normalize,
