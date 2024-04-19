07f3c611754df217a146d91e1a5ed6206dac0367
Taheera Ahmed <taheera@hotmail.com>
2024-04-19 12:16:51 +0200

    new training 2024-04-19-12:16:35

--- 01-multi-label/src/models/set_optimizer.py
+++ 01-multi-label/src/models/set_optimizer.py
@@ -2,9 +2,29 @@ from models import ModelConfig
 from utils import FileManager
 import torch
 
+def get_classifying_head(model_config: ModelConfig) -> torch.nn.Module:
+    model_arg = model_config.model_arg
+    model = model_config.model
+
+    if model_arg == 'resnet50':
+        classifying_head = model.fc.parameters()
+    elif model_arg == 'alexnet' or 'densenet121' or 'efficientnet':
+        classifying_head = model.classifier.parameters()
+    elif model_arg == 'vit':
+        classifying_head = model.heads.parameters()
+
+    return classifying_head
+    
+
+
+
 def set_optimizer(model_config: ModelConfig, file_manager: FileManager) -> torch.optim.Optimizer:
     model = model_config.model
     optimizer_arg = model_config.optimizer_arg
+
+    # step 1: TODO COMMENT OUT
+    classifying_head = get_classifying_head(model_config)
+
     if optimizer_arg == 'adam':
         optimizer = torch.optim.Adam(
             model.parameters(),
@@ -14,7 +34,7 @@ def set_optimizer(model_config: ModelConfig, file_manager: FileManager) -> torch
     elif optimizer_arg == 'sgd':
         optimizer = torch.optim.SGD(
             # step 1: TODO ONLY LAST LAYER
-            model.fc.parameters(),
+            classifying_head,
             #model.parameters(),
             lr=model_config.learning_rate,
         )
