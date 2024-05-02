f63845075495c78674b43fc34e71cdc4320dac43
Taheera Ahmed <taheera@hotmail.com>
2024-05-02 12:09:03 +0200

    clean up, move classifying head to own func, comment out vit/swin

--- 01-multi-label/src/models/set_model.py
+++ 01-multi-label/src/models/set_model.py
@@ -1,98 +1,80 @@
-from torchvision.models import resnet50, resnet34, alexnet, vit_b_16, densenet121, efficientnet_b2
+from torchvision.models import resnet50, alexnet, vit_b_16, densenet121, efficientnet_b1
 from transformers import Swinv2ForImageClassification
 import torch
 from torch import nn
+
+def classifying_head(in_features: int, num_labels: int):
+    return nn.Sequential(
+        nn.Linear(in_features=in_features, out_features=512),
+        nn.ReLU(),
+        nn.Dropout(p=0.2),
+        nn.Linear(in_features=512, out_features=256),
+        nn.ReLU(),
+        nn.Dropout(p=0.2),
+        nn.BatchNorm1d(num_features=256),
+        nn.Linear(256, num_labels),
+    )
 
 
 def set_model(model_arg: str, num_labels: int, labels: list):
 
     if model_arg == 'resnet50':
-        model = resnet50(weights='IMAGENET1K_V1')
+        model = resnet50(weights='IMAGENET1K_V2')
         img_size = int(224) 
 
-        model.fc = nn.Sequential(
-            nn.Dropout(p=0.2),
-            nn.Linear(in_features=model.fc.in_features, out_features=256),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_features=256),
-            nn.Linear(256, num_labels),
-        )
+        model.fc = classifying_head(model.fc.in_features, num_labels)
 
-    elif model_arg == 'resnet34':
-        model = resnet34(weights='IMAGENET1K_V1')
-        model.fc = torch.nn.Linear(in_features=model.fc.in_features, out_features=num_labels, bias=True)
-        img_size = int(224)     
+        # step 1: TODO ONLY LAST LAYER
+        # for param in model.parameters():
+        #     param.requires_grad = True
+
+        # for param in model.fc.parameters():
+        #     param.requires_grad = False
 
     elif model_arg == 'swin':
-        id2label = {id: label for id, label in enumerate(labels)}
-        label2id = {label: id for id, label in id2label.items()}
+        # id2label = {id: label for id, label in enumerate(labels)}
+        # label2id = {label: id for id, label in id2label.items()}
 
-        img_size = int(256) 
+        # img_size = int(256) 
 
-        model = Swinv2ForImageClassification.from_pretrained(
-            "microsoft/swinv2-tiny-patch4-window8-256", 
-            num_labels=num_labels,
-            id2label=id2label,
-            label2id=label2id,
-            ignore_mismatched_sizes=True
-        )
+        # model = Swinv2ForImageClassification.from_pretrained(
+        #     "microsoft/swinv2-tiny-patch4-window8-256", 
+        #     num_labels=num_labels,
+        #     id2label=id2label,
+        #     label2id=label2id,
+        #     ignore_mismatched_sizes=True
+        # )
         raise NotImplementedError("Swin Transformer is not yet supported")
     
     elif model_arg == "vit":
-        img_size = int(224) 
-        model = vit_b_16()
+        # img_size = int(224) 
+        # model = vit_b_16(weights="IMAGENET1K_V1")
 
-        model.heads = nn.Sequential(
-            nn.Dropout(p=0.2),
-            nn.Linear(768, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_labels),
-        )
+        # model.heads = nn.Sequential(
+        #     nn.Dropout(p=0.2),
+        #     nn.Linear(768, 128),
+        #     nn.ReLU(),
+        #     nn.Linear(128, num_labels),
+        # )
+        raise NotImplementedError("Vision Transformer is not yet supported")
 
     elif model_arg == "alexnet": 
         model = alexnet(weights="DEFAULT")
         img_size = int(224) 
     
-        model.classifier = nn.Sequential(
-            nn.Dropout(p=0.2),
-            nn.Linear(256 * 6 * 6, 128),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_features=128),
-            nn.Linear(128, num_labels),
-        )
+        model.classifier = classifying_head(256*6*6, num_labels)
+
     elif model_arg == "densenet121":
         img_size = int(224) 
         model = densenet121(weights="IMAGENET1K_V1")
         
-        model.classifier = nn.Sequential(
-            nn.Dropout(p=0.2),
-            nn.Linear(1024, 128),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_features=128),
-            nn.Linear(128, num_labels),
-        )
+        model.classifier = classifying_head(1024, num_labels)
     
     elif model_arg == "efficientnet":
         img_size = int(260)
-        model = efficientnet_b2(weights="IMAGENET1K_V1")
+        model = efficientnet_b1(weights="IMAGENET1K_V2")
 
-        model.classifier = nn.Sequential(
-            nn.Dropout(p=0.3),
-            nn.Linear(1408, 512),
-            nn.BatchNorm1d(num_features=512),
-            nn.ReLU(),
-            nn.Linear(512, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_labels),
-        )
-    elif model_arg == "chexnet":
-        img_size = int(224)
-        model = densenet121(weights="IMAGENET1K_V1")
-
-        # using sigmoid else where
-        model.classifier = nn.Sequential(
-            nn.Linear(model.classifier.in_features, num_labels)
-        )
+        model.classifier = classifying_head(1280, num_labels)
 
     else:
         raise ValueError(f'Invalid model argument: {model_arg}')
